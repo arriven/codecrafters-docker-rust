9df2167b08a70f92d6252022c94e2cdfe9f76db1
arriven <20084245+Arriven@users.noreply.github.com>
2022-07-24 21:09:41 +0300

    second stage

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     // Uncomment this block to pass the first stage!
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
@@ -13,8 +10,10 @@ fn main() {
         .unwrap();
     
     if output.status.success() {
-        std::io::stdout().write_all(&output.stdout).unwrap();
-        std::io::stderr().write_all(&output.stderr).unwrap();
+        let std_out = std::str::from_utf8(&output.stdout).unwrap();
+        println!("{}", std_out);
+        let std_err = std::str::from_utf8(&output.stderr).unwrap();
+        eprintln!("{}", std_err);
     } else {
         std::process::exit(1);
     }
