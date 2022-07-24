f08b06a4129721353f3c0f1dd3b301afcf37d1df
arriven <20084245+Arriven@users.noreply.github.com>
2022-07-24 22:08:09 +0300

    stage 4

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ fn main() -> std::io::Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    std::fs::create_dir("/sandbox").unwrap();
-    std::fs::create_dir("/sandbox/dev").unwrap();
-    std::fs::copy(command, "/sandbox/app").unwrap();
-    std::fs::File::create("/sandbox/dev/null").unwrap();
+    std::fs::create_dir("/sndbx").unwrap();
+    std::fs::create_dir("/sndbx/dev").unwrap();
+    std::fs::copy(command, "/sndbx/app").unwrap();
+    std::fs::File::create("/sndbx/dev/null").unwrap();
     let code = unsafe {
-        libc::chroot("/sandbox\0".as_ptr() as *const i8)
+        libc::chroot("/sndbx\0".as_ptr() as *const i8)
     };
     if code != 0 {
         return Err(std::io::Error::last_os_error());
