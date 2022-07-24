5fce73c79d93bfa868b1d9b34a5e60a5d60e3d25
arriven <20084245+Arriven@users.noreply.github.com>
2022-07-24 21:45:34 +0300

    stage 4

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-
 use std::io::{self, Write};
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
-fn main() {
+fn main() -> std::io::Result<()> {
     // Uncomment this block to pass the first stage!
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let output = std::process::Command::new(command)
+    std::fs::create_dir("/sandbox")?;
+    std::fs::create_dir("/sandbox/dev")?;
+    std::fs::copy(command, "/sandbox/app")?;
+    std::fs::File::create("/sandbox/dev/null")?;
+    std::os::unix::fs::chroot("/sandbox")?;
+    std::env::set_current_dir("/")?;
+    let output = std::process::Command::new("/app")
         .args(command_args)
         .output()
         .unwrap();
@@ -16,4 +21,5 @@ fn main() {
     if let Some(code) = output.status.code() {
         std::process::exit(code);
     }
+    Ok(())
 }
