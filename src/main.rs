3dd440c288c3ab1a81dfd0e03560ade6086db728
arriven <20084245+Arriven@users.noreply.github.com>
2022-07-30 18:25:07 +0300

    move some functions out

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,26 @@ fn main() -> std::io::Result<()> {
     let command = &args[3];
     let command_args = &args[4..];
 
-    if std::path::Path::new("/sandbox").exists() {
-        std::fs::remove_dir_all("/sandbox").unwrap();
-    }
-    std::fs::create_dir("/sandbox").unwrap();
-    std::fs::create_dir("/sandbox/dev").unwrap();
+    init_sandbox("/sandbox").unwrap();
     std::fs::copy(command, "/sandbox/app").unwrap();
-    std::fs::File::create("/sandbox/dev/null").unwrap();
+    chroot("/sandbox").unwrap();
+    
+    let output = std::process::Command::new("/app")
+        .args(command_args)
+        .output()
+        .unwrap();
+
+    io::stdout().write_all(&output.stdout).unwrap();
+    io::stderr().write_all(&output.stderr).unwrap();
+    if let Some(code) = output.status.code() {
+        std::process::exit(code);
+    }
+    Ok(())
+}
+
+fn chroot(path: &str) -> std::io::Result<()> {
     let code = unsafe {
-        libc::chroot("/sandbox\0".as_ptr() as *const i8)
+        libc::chroot(path.as_ptr().cast::<i8>())
     };
     if code != 0 {
         return Err(std::io::Error::last_os_error());
@@ -26,16 +37,15 @@ fn main() -> std::io::Result<()> {
     if code != 0 {
         return Err(std::io::Error::last_os_error());
     }
-    std::env::set_current_dir("/")?;
-    let output = std::process::Command::new("/app")
-        .args(command_args)
-        .output()
-        .unwrap();
+    std::env::set_current_dir("/")
+}
 
-    io::stdout().write_all(&output.stdout).unwrap();
-    io::stderr().write_all(&output.stderr).unwrap();
-    if let Some(code) = output.status.code() {
-        std::process::exit(code);
+fn init_sandbox(path: &str) -> std::io::Result<()> {
+    if std::path::Path::new(path).exists() {
+        std::fs::remove_dir_all(path).unwrap();
     }
+    std::fs::create_dir(path).unwrap();
+    std::fs::create_dir(path.to_string() + "/dev").unwrap();
+    std::fs::File::create(path.to_string() + "/dev/null").unwrap();
     Ok(())
 }
