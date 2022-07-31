f6f0e4f108c4ed40436315d764ab0abedf1991d3
arriven <20084245+Arriven@users.noreply.github.com>
2022-07-31 20:01:28 +0300

    some cleanup

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() -> std::io::Result<()> {
     init_sandbox("sandbox").unwrap();
     pull(image, "sandbox").unwrap();
     chroot("sandbox").unwrap();
-    
+
     let output = std::process::Command::new(command)
         .args(command_args)
         .output()
@@ -26,66 +26,79 @@ fn main() -> std::io::Result<()> {
 
 fn pull(image: &str, path: &str) -> std::io::Result<()> {
     let parts = image.split(":").collect::<Vec<&str>>();
-    let repo = parts[0];
-    let tag = if parts.len() < 2 {
-        "latest"
-    }  else {
-        parts[1]
+    let repo = if parts[0].contains("/") {
+        parts[0].to_string()
+    } else {
+        format!("library/{}", parts[0])
     };
-    //https://registry.hub.docker.com/v2/library/ubuntu/manifests/latest
-    let response = reqwest::blocking::get(&format!("https://registry.hub.docker.com/v2/library/{}/manifests/{}", repo, tag)).unwrap();
-    if response.status() == reqwest::StatusCode::UNAUTHORIZED {
-        let auth_header = response.headers().get("Www-Authenticate").unwrap().to_str().unwrap();
-        let oauth_endpoint = parse_oauth_header(auth_header);
+    let tag = if parts.len() < 2 { "latest" } else { parts[1] };
 
-        let response = reqwest::blocking::get(&format!("{}?service={}&scope={}", oauth_endpoint.realm, oauth_endpoint.service, oauth_endpoint.scope)).unwrap();
-        
-        let body: serde_json::Value = response.json().unwrap();
-        if let serde_json::Value::String(ref token) = body["token"] {
-            let client = reqwest::blocking::Client::new();
-            let manifest: serde_json::Value = client.get(&format!("https://registry.hub.docker.com/v2/library/{}/manifests/{}", repo, tag))
-                .header("Authorization", format!("Bearer {}", token)).send().unwrap().json().unwrap();
-            if let serde_json::Value::Array(ref fs_layers) = manifest["fsLayers"] {
-                for fs_layer in fs_layers.iter() {
-                    if let serde_json::Value::String(ref digest) = fs_layer["blobSum"] {
-                        let response = client.get(&format!("https://registry.hub.docker.com/v2/library/{}/blobs/{}", repo, digest))
-                            .header("Authorization", format!("Bearer {}", token)).send().unwrap();
-                        let layer = response.bytes().unwrap();
-                        let filename = digest.replace(":", "_");
-                        let mut file = std::fs::File::create(format!("{}.tar", filename))?;
-                        file.write_all(&layer)?;
-                        let output = std::process::Command::new("tar")
-                            .args(["xf", &format!("{}.tar", filename), "-C", path])
-                            .output()
-                            .unwrap();
-                        io::stdout().write_all(&output.stdout).unwrap();
-                        io::stderr().write_all(&output.stderr).unwrap();
-                        assert!(output.status.success());
-                    }
+    let registry_uri = format!(
+        "https://registry.hub.docker.com/v2/{}/manifests/{}",
+        repo, tag
+    );
+
+    let response = reqwest::blocking::get(&registry_uri).unwrap();
+    assert!(
+        response.status() == reqwest::StatusCode::UNAUTHORIZED,
+        "we don't want to handle all cases since it's just a challenge and not a prod app"
+    );
+    let auth_header = response
+        .headers()
+        .get("Www-Authenticate")
+        .unwrap()
+        .to_str()
+        .unwrap();
+    let oauth_values = auth_header
+        .trim_start_matches("Bearer ")
+        .split(",")
+        .map(parse_oauth_value)
+        .collect::<std::collections::HashMap<&str, &str>>();
+
+    let body: serde_json::Value = reqwest::blocking::get(&format!(
+        "{}?service={}&scope={}",
+        oauth_values.get("realm").unwrap(),
+        oauth_values.get("service").unwrap().to_string(),
+        oauth_values.get("scope").unwrap()
+    ))
+    .unwrap()
+    .json()
+    .unwrap();
+
+    if let serde_json::Value::String(ref token) = body["token"] {
+        let client = reqwest::blocking::Client::new();
+        let manifest: serde_json::Value = client
+            .get(&registry_uri)
+            .header("Authorization", format!("Bearer {}", token))
+            .send()
+            .unwrap()
+            .json()
+            .unwrap();
+        if let serde_json::Value::Array(ref fs_layers) = manifest["fsLayers"] {
+            for fs_layer in fs_layers.iter() {
+                if let serde_json::Value::String(ref digest) = fs_layer["blobSum"] {
+                    let response = client
+                        .get(&format!(
+                            "https://registry.hub.docker.com/v2/{}/blobs/{}",
+                            repo, digest
+                        ))
+                        .header("Authorization", format!("Bearer {}", token))
+                        .send()
+                        .unwrap();
+                    let layer = response.bytes().unwrap();
+                    let filename = digest.replace(":", "_");
+                    let mut file = std::fs::File::create(format!("{}.tar", filename))?;
+                    file.write_all(&layer)?;
+                    let output = std::process::Command::new("tar")
+                        .args(["xf", &format!("{}.tar", filename), "-C", path])
+                        .output()
+                        .unwrap();
+                    assert!(output.status.success());
                 }
-            } else {
-                println!("wrong type");
             }
         }
     }
-    //https://auth.docker.io/token\?service\=registry.docker.io\&scope\=repository:library/ubuntu:pull
     Ok(())
-}
-
-#[derive(Debug)]
-struct OauthEndpoint {
-    realm: String,
-    service: String,
-    scope: String,
-}
-
-fn parse_oauth_header(header: &str) -> OauthEndpoint {
-    let auth_values = header.trim_start_matches("Bearer ").split(",").map(parse_oauth_value).collect::<std::collections::HashMap<&str,&str>>();
-    OauthEndpoint{
-        realm: auth_values.get("realm").unwrap().to_string(),
-        service: auth_values.get("service").unwrap().to_string(),
-        scope: auth_values.get("scope").unwrap().to_string(),
-    }
 }
 
 fn parse_oauth_value(value: &str) -> (&str, &str) {
@@ -97,16 +110,12 @@ fn parse_oauth_value(value: &str) -> (&str, &str) {
 }
 
 fn chroot(path: &str) -> std::io::Result<()> {
-    let code = unsafe {
-        libc::chroot(format!("{}\0", path).as_ptr().cast::<i8>())
-    };
+    let code = unsafe { libc::chroot(format!("{}\0", path).as_ptr().cast::<i8>()) };
     if code != 0 {
         return Err(std::io::Error::last_os_error());
     }
 
-    let code = unsafe {
-        libc::unshare(libc::CLONE_NEWPID)
-    };
+    let code = unsafe { libc::unshare(libc::CLONE_NEWPID) };
     if code != 0 {
         return Err(std::io::Error::last_os_error());
     }
